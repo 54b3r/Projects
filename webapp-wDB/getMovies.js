02967fa97d786c8f4f7890679e3ba83fe29eea19
Marvin Matos <mmatos@netwireps.com>
2018-05-27 16:03:03 -0400

    update

--- webapp-wDB/getMovies.js
+++ webapp-wDB/getMovies.js
@@ -8,35 +8,29 @@ class GetMovies {
         const db = mongoose.connect('mongodb://127.0.0.1:27017/vidly-app')
         const db1 = mongoose.connection
 
-        db1.on('error', console.error.bind(console, 'Conncection error:...'));
-        db1.once('open', () => {
-            console.log('Connection to the movies db has been sucessfull...');
-            var movieSchema = new Schema({
-                movieName: {type: String, required:true},
-                movieDetails: {
-                    rating: String,
-                    length: String,
-                    releaseDate: Date,
-                    description: String,
-                    directors: [String],
-                    mainCast: [String],
-                    fullCast: [String],
-                    genre: {type: String, required:true},
-                    subGenres: [String],
-                    imbd_url: String,
-                },
-                },{collection: 'movies' })
+        var movieSchema = new Schema({
+            movieName: {type: String, required:true},
+            movieDetails: {
+                rating: String,
+                length: String,
+                releaseDate: Date,
+                description: String,
+                directors: [String],
+                mainCast: [String],
+                fullCast: [String],
+                genre: {type: String, required:true},
+                subGenres: [String],
+                imbd_url: String,
+            },
+        },{collection: 'movies' })
 
-            const queryGetMovies = db1.model('queryGetMovies', movieSchema);
-            queryGetMovies.find((err,docs) => {
-                if(err) return console.error(err);
-                return console.log('Below are your results:...');
-                return console.log(docs);
+        const queryGetMovies = db1.model('Movies', movieSchema);
+        queryGetMovies.find((err,docs) => {
+            if(err) return console.error(err);
+            return console.log('Below are your results:...');
+            return console.log(docs);
             });
-        })
-        db1.on('close', () => {
-            console.log("Sonnection is being terminated:...");
-        });
+
         
     }
 }
@@ -67,7 +61,7 @@ function getMovies() {
             },
             },{collection: 'movies' })
 
-        const queryGetMovies = db1.model('queryGetMovies', movieSchema);
+        const queryGetMovies = db1.model('Movies', movieSchema);
         queryGetMovies.find((err,docs) => {
             if(err) return console.error(err);
             return send('Below are your results:...');
