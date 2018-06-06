70e35b1e51fbce2a847c9cb89de5868deb324d67
Marvin Matos <mmatos@netwireps.com>
2018-06-06 13:06:20 -0400

    added route.get for /register endpoint just console logging for now

--- MEAN-Auth-App/routes/users.js
+++ MEAN-Auth-App/routes/users.js
@@ -1,2 +1,8 @@
 const express = require('express');
-const 
+const router = express.Router();
+
+//Register endpoint for user registration
+//route to /register endpoint, console log for now
+router.get('/register', (req, res, next) => {
+    res.json('REGISTER ENDPOINT HAS BEN REACHED');
+});
