6c373a81de8700d93fcc8688826c8d25405b42d6
Marvin Matos <mmatos@netwireps.com>
2018-06-06 13:30:04 -0400

    added the remaing routes for the /users endpoint in /route/users.js

--- MEAN-Auth-App/routes/users.js
+++ MEAN-Auth-App/routes/users.js
@@ -4,5 +4,17 @@ const router = express.Router();
 //Register endpoint for user registration
 //route to /register endpoint, console log for now
 router.get('/register', (req, res, next) => {
-    res.json('REGISTER ENDPOINT HAS BEN REACHED');
+    res.send('REGISTER ENDPOINT HAS BEN REACHED');
 });
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE ENDPOINT HAS BEN REACHED');
+});
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE ENDPOINT HAS BEN REACHED');
+});
+//validate route to see if token matches
+router.get('/validate', (req, res, next) => {
+    res.send('VALIDATE ENDPOINT HAS BEN REACHED');
+});
+
+module.exports = router;
