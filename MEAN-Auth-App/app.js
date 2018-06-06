cdb52d254b55f083c1afb8ec0d68d088dd39d5fb
Marvin Matos <mmatos@netwireps.com>
2018-06-06 12:57:36 -0400

    configured app.use('/users', users); requiredthe routes/users module

--- MEAN-Auth-App/app.js
+++ MEAN-Auth-App/app.js
@@ -3,13 +3,16 @@ const path = require('path');
 const bodyParsder = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');   
 
 const app = express();
+const users = require('./routes/users');
 //CORS middleware
 app.use(cors());
 //Body Parser Middleware
 app.use(bodyParsder.json())
+//says anything passes to /users/xxx endpoint will go 2 that users file
+app.use('/users', users);
 
 const port = process.env.PORT || 3000
 
