07b386f502e031573954248d519b0d32fa3da919
Marvin Matos <mmatos@netwireps.com>
2018-06-24 04:09:50 -0400

    reordered the code +raw input to save the filename to local HDD +print stmts

--- Python/getRJ.py
+++ Python/getRJ.py
@@ -1,18 +1,27 @@
-import requests
+import requests, bs4
 
-res = requests.get ('https://automatetheboringstuff.com/files/rj.txt')
+res = requests.get ('http://nostarch.com')
 try:
     res.raise_for_status()
 except Exception as exc:
     print('There was a problem: $s' %(exc))
 
-len(res.text)
-print(res.text[:250])
+#Write file to HDD(will be in current workingdir)
+#Write code to take raw input to name the file to save
+#Store the the raw output in usr_inpt
+def saveWebpageToHdd():
+    print('Starting webpage download...')
+    filename = raw_input('Please enter the desired filename...\n: ')
+    saveFile = open(filename, 'wb')
+    for chunk in res.iter_content(100000):
+        saveFile.write(chunk)
+    saveFile.close()
 
-playFile = open('RomeoAndJuliet.txt', 'wb')
-for chunk in res.iter_content(100000):
-    playFile.write(chunk)
-playFile.close()
+saveWebpageToHdd()
+print('Character count: '+ str(len(res.text)))
+print('This is the first 250 characters of the file: %s\n' %res.text[:250])
+
+
 
 
 
