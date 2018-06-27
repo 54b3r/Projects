f53765b3d76909080a429adc55fb390fd9bb624e
Marvin Matos <mmatos@netwireps.com>
2018-06-27 06:15:33 -0400

    added v5000 url

--- Python/ISOC-Projects/login.py
+++ Python/ISOC-Projects/login.py
@@ -7,11 +7,18 @@ un=raw_input("Please enter the desired Username to login with: ")
 pin=getpass.getpass("Please enter the desired password to login with: ")
 
 payload = urllib.urlencode({'USERNAME': un, 'PIN': pin})
-url = 'http://sykpcasd12ap1v:80/CAisd/pdmweb.exe'
-full_url = url + '?' + payload
-response = urllib2.urlopen(full_url)
-webbrowser.open(full_url)
+ibmPayload = urllib.urlencode({'':un})
+
+caURL = 'http://sykpcasd12ap1v:80/CAisd/pdmweb.exe'
+caLoginURL = caURL + '?' + payload
+v5000URL = "https://10.170.82.207/login"
+v5000LoginURL = v5000URL + '?' + ibmPayload
+
+
+webbrowser.open(caLoginURL)
+webbrowser.open_new_tab(v5000LoginURL)
 
 def saveResponseToDisk():
+    response = urllib2.urlopen(caLoginURL)
     with open("results.html", "w") as f:
         f.write(response.read())
