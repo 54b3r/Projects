ecf5c601a6381247bc69b3853ce90f2a640ae438
Marvin Matos <mmatos@netwireps.com>
2018-06-27 05:59:08 -0400

    alterations to code layout

--- Python/ISOC-Projects/login.py
+++ Python/ISOC-Projects/login.py
@@ -6,7 +6,6 @@ print("Now Opening and Loggin into CA Servicedesk")
 un=raw_input("Please enter the desired Username to login with: ")
 pin=getpass.getpass("Please enter the desired password to login with: ")
 
-
 payload = urllib.urlencode({'USERNAME': un, 'PIN': pin})
 url = 'http://sykpcasd12ap1v:80/CAisd/pdmweb.exe'
 full_url = url + '?' + payload
